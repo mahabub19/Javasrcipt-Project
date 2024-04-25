6c69de012b184985792d068443514b0d3aee5860
mahabub-alam <nishat.man2017@gmail.com>
2024-04-25 20:06:44 +0600

    25/4

--- Practice/script.js
+++ Practice/script.js
@@ -31,6 +31,7 @@ btn.addEventListener('mouseout', out);
 function validate() {
     let x1 = document.forms["frm"]["first"].value;
     let x2 = document.forms["frm"]["second"].value;
+    // let x = document.forms["frm"].childNode[1].childNode;
     if (x1 == "" || x2 == "") {
         alert("Must fill the box");
         return false;
@@ -40,16 +41,21 @@ function validate() {
     }
 }
 
+// function changeFont() {
+//     let x1 = document.forms["frm"]["first"].value;
+//     // let x2 = document.forms["frm"]["second"].value;
+//     x1 = x1.toUpperCase();
+//     // x2 = x2.toUpperCase();
+// }
+
 document.getElementById('submit').addEventListener('click', function () {
     let ob_collections = document.forms["frm"];
-    console.log(ob_collections);
     let text = ob_collections["first"].value + " " + ob_collections["second"].value;
 
     document.getElementById('full_name').innerHTML = text;
 });
 
 document.getElementById('move').addEventListener('click', function () {
-    console.log("clicked");
     let box = document.getElementById("inner");
     let pos = 0;
     let id = setInterval(animate, 1);
@@ -63,5 +69,53 @@ document.getElementById('move').addEventListener('click', function () {
     }
 })
 
+// let element = document.getElementById("id1");
+
+// element.addEventListener('click', function () {
+//     element.style.color = "red";
+//     element.style.fontSize = "50px"
+// })
+
+function changeText(element) {
+    element.style.color = "red";
+    element.style.fontSize = "50px"
+}
+
+
+let p_tag = document.createElement("p");
+let node = document.createTextNode("This is added by javasrcipt");
+
+p_tag.appendChild(node);
+
+document.getElementById("new").appendChild(p_tag);
+
+
+
+
+
+let grid = document.getElementById("grid");
+
+let htmlCollections = grid.querySelectorAll("button");
+console.log(htmlCollections[0].innerHTML);
+
+let m = [];
+
+function print() {
+    let text = "";
+    for (let i = 0; i < m.length; i++){
+        text += i + 1 + ". " + m[i] + '\n'; 
+    }
+    // console.log(text);
+    document.getElementById("print").innerHTML = text;
+}
+
+for (let i = 0; i < htmlCollections.length; i++){
+    htmlCollections[i].addEventListener('click', function () {
+        m.push(htmlCollections[i].innerHTML);
+        htmlCollections[i].style.background = "yellow";
+        console.log(m.length);
+        print();
+    })
+}
 
 
